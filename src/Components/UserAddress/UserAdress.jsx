dc3a9a1e0e2004ae2398f8e8818cbd4ca9d177c1
e659 <emoomohamed419@gmail.com>
2024-01-08 22:39:29 +0200

    fix

--- src/Components/UserAddress/UserAdress.jsx
+++ src/Components/UserAddress/UserAdress.jsx
@@ -41,7 +41,7 @@ export default function UserAdress() {
           setIsLoading(true);
           let res = await onlinePayment(
             cartId,
-            "https://e659.github.io/FreshCart/",
+            "https://e659.github.io/FreshCart",
             values
           );
           console.log(res);
